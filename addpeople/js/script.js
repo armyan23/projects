553d69e911a3a47589252af582d3d9b032daba81
armyan23 <arm.davtyan1029@gmail.com>
2022-04-11 21:10:15 +0400

    Editing Add people new version but not upload img new img

--- addpeople/js/script.js
+++ addpeople/js/script.js
@@ -13,10 +13,9 @@ document.getElementById('formWindowBtn').addEventListener('click', function(){
 })
 document.getElementById('closeWindow').addEventListener('click',function(e) {
     document.getElementById('formWindow').style.display = 'none'
-    toggle = !toggle 
-    console.log('poo')
-
+    toggle = !toggle
 })
+
 // Data Registr
 function timeReg(par){
     let date = new Date()
@@ -35,7 +34,7 @@ function timeReg(par){
 }
 
 
-// icons EDIT, DELET, INFO
+// icons  DELET and INFO
 function icons(icon, td) {
     const img = document.createElement("img");
     
@@ -48,123 +47,122 @@ function icons(icon, td) {
            let thisId = img.parentElement.parentElement.firstChild.textContent
            localStorage.removeItem(`id_${thisId}`)
         }
-        else if(icon === "edit") {
-               console.log(img.parentElement.parentElement.childNodes)
-        //     const imgOk= document.createElement("img");
-        //     const imgCa = document.createElement("img");
-        //     imgOk.style.marginRight = "15px"
-        //     imgCa.classList.add("icons")
-        //     imgOk.classList.add("icons")
-            
-        //     imgCa.setAttribute("src", `img/cancele.png`)
-        //     imgOk.setAttribute("src", `img/okay.png`)
-        //     td.append(imgOk, imgCa)
-        //     img.hidden = true
-            // console.log(Array.from(img.parentElement.parentElement.childNodes))
-            //  Array.from(img.parentElement.parentElement.childNodes).forEach((elem,index,arr) => {
-                
-            //     if(index > 1 && index < arr.length-3){
-            //         let inp = document.createElement("input")
-            //         inp.value = arr[index].textContent
-            //         // inp.value = arr[1].textContent
-            //         inp.name = form.children[index-2].name
-            //         elem.textContent = ""
-            //         inp.classList.add("editInput")
-            //         inp.setAttribute("type","text")
-            //         elem.appendChild(inp)
-            //     }
-
-            // })
-            //  // FUNCTION FILL TD general CACLE and EDIT
-            //  function fillGeneral(arrValue) {
-            //     img.hidden = false
-            //     imgOk.hidden = true
-            //     imgCa.hidden = true
-
-            //     Array.from(img.parentElement.parentElement.childNodes).forEach((elem,index,arr) => {
-            //         if(index > 1 && index < arr.length-3){
-            //             elem.childNodes[0].remove()
-            //             elem.textContent = arrValue[index-2]
-            //         }
-            //     })
-            // }
-            //EDIT EVENT 
-        //    imgOk.addEventListener("click", function(e) {
-        //         let thisId = td.parentElement.childNodes[0].textContent
-        //         let arrValue = []
-        //         let editObject =  Array.from(td.parentElement.childNodes).reduce((acc,elem) => {
-        //             if(elem.childNodes[0].name){
-        //                 let editVal = elem.childNodes[0].value
-        //                 let editName = elem.childNodes[0].name
-        //                 acc[editName] = editVal
-        //                 arrValue.push(editVal)
-        //             }
-        //             return acc
-        //         },{})
-                
-        //         let editing = JSON.parse(localStorage.getItem(`id_${thisId}`))
-        //         for(let o in editing){
-        //             for(let x in editObject){
-        //                 if(x == o) {
-        //                     editing[o] = editObject[x]
-        //                 }
-        //             }
-        //         }
-        //         localStorage.setItem(`id_${thisId}`, JSON.stringify(editing))
-        //         fillGeneral(arrValue)
-        //    })
-        //    CANCLE ICON EVENT
-        //    imgCa.addEventListener("click", function() {
-                
-        //         let arrValue = []
-        //         let editObject =  Array.from(td.parentElement.childNodes).map((elem) => {
-        //             if(elem.childNodes[0].name){
-        //                 let editVal = elem.childNodes[0].value
-        //                 arrValue.push(editVal)
-        //             }
-        //         })
-        //         fillGeneral(arrValue)
-
-        //    })
-        }
         else if(icon === "info") {
-            let thisObj = JSON.parse(localStorage.getItem(`id_${img.parentElement.parentElement.firstChild.textContent}`))
+            let thisObj = JSON.parse(localStorage.getItem(`id_${img.parentElement.parentElement.firstElementChild.textContent}`))
             const infoDiv = document.querySelector(".infoDiv")
+            const infoContent = document.querySelector(".infoContent")
+            infoContent.textContent = ''
             infoDiv.hidden = false
             
-            const infoImg =  document.querySelector(".infoDiv img")
+            const infoImg = document.createElement('img')
             // infoImg.setAttribute("src", `img/${thisObj.img}`)
             URL.revokeObjectURL(infoImg.src)
             infoImg.src = thisObj.img
+            infoContent.prepend(infoImg)
 
+            let arrayFirstTd = ['First Name:','Last Name:','Tel:','Old:','Address:','Add date:','Add time:','Edit:']
+            let arraySecondTd = [thisObj.name,thisObj.surname,thisObj.tel,thisObj.old,thisObj.company,thisObj.data,thisObj.time]
 
-            const spanAll = document.querySelectorAll(".infoDiv span")
+            const tableInfo = document.createElement('table')
+
+            const imgEdit= document.createElement("img");
+            const imgOk= document.createElement("img");
+            const imgCa = document.createElement("img");
+            imgEdit.classList.add("icons")
+            imgOk.classList.add("icons")
+            imgOk.style.display = 'none'
+            imgOk.style.marginRight = "15px"
+            imgCa.classList.add("icons")
+            imgCa.style.display = 'none'
             
-            spanAll[0].textContent = `First Name: ${thisObj.name}`
-            spanAll[1].textContent = `Last Name:  ${thisObj.surname}`
-            spanAll[2].textContent = `Old:        ${thisObj.old}`
-            spanAll[3].textContent = `Tel:        ${thisObj.tel}`
-            spanAll[4].textContent = `Adress:     ${thisObj.company}`
-            spanAll[5].textContent = `Add date:   ${thisObj.data} `
-            spanAll[6].textContent = `Add time:   ${thisObj.time}`
-            spanAll[7].textContent = `Edit changes...`
+            imgEdit.setAttribute("src", `img/edit.png`)
+            imgOk.setAttribute("src", `img/okay.png`)
+            imgCa.setAttribute("src", `img/cancele.png`)
 
-            const editIcon = document.createElement("img");
-    
-            editIcon.setAttribute("src", `img/edit.png`)
-            editIcon.classList.add("icons")
-            // spanAll[7].append(editIcon)
-            console.log(thisObj)
+            for(let i = 0; i < arrayFirstTd.length; i++ ){
+                const tr = document.createElement('tr')
+                const tdFirst = document.createElement('td')
+                const tdSecond = document.createElement('td')
+                tdFirst.textContent = arrayFirstTd[i]
+                tdSecond.textContent = arraySecondTd[i]
+                if(i== arrayFirstTd.length-1) tdSecond.append(imgEdit,imgOk, imgCa)
+                tr.append(tdFirst,tdSecond)
+                tableInfo.append(tr)
+            }
+            infoContent.append(tableInfo)
+
+            const editIcons = document.querySelectorAll('.infoContent  img')
 
 
-            const infoExit = document.querySelector(".infoExit")
-            infoExit.addEventListener("click", function(e){
-                infoDiv.hidden =true
+            const trAll = document.querySelectorAll(".infoDiv table tr")
+            let parent = e.target.parentElement.parentElement
+            infoDiv.addEventListener("click", function(e){
+                if(e.target.parentElement.classList.contains('infoExit')) infoDiv.hidden =true
+                if(e.target == editIcons[1])  editFunction(thisObj,trAll, editIcons)
+                if(e.target == editIcons[2])  okayEditing(thisObj,trAll, editIcons,parent,infoDiv)
+                if(e.target == editIcons[3])  cancaleEditing(thisObj,trAll, editIcons)
             })
+
         }
     })
     td.appendChild(img)
 
+}
+
+// Edit Function 
+function editFunction(props,trAll,editIcons) {
+    editIcons[1].style.display = 'none'
+    editIcons[2].style.display = 'inline'
+    editIcons[3].style.display = 'inline'
+
+    // Creating INPUTS and put in td with value 
+    Object.entries(props).map(function(elem,index) {
+        if(index > 1 && index < 7) {
+            let inp = document.createElement("input")
+            inp.classList.add("editInput")
+            inp.value = trAll[index-2].lastElementChild.textContent
+            trAll[index-2].lastElementChild.textContent = ''
+            trAll[index-2].lastElementChild.appendChild(inp)
+        }
+    })
+
+}
+
+// OKay Edit editing 
+function okayEditing(props,trAll,editIcons,parent,infoDiv) {
+    infoDiv.hidden = true
+    editIcons[1].style.display = 'inline'
+    editIcons[2].style.display = 'none'
+    editIcons[3].style.display = 'none'
+    let editObject = Object.entries(props).map(function(elem,index) {
+        if(index > 1 && index < 7) {
+            elem[1] =  trAll[index-2].lastElementChild.lastElementChild.value
+            trAll[index-2].lastElementChild.textContent = elem[1]
+            // console.log(elem[1])
+            // if(index < 5) console.log(parent.children[index].textContent,`ooop`,elem[1])
+            if(index < 5) parent.children[index].textContent = elem[1]
+            return elem
+        }
+        else return elem 
+    })
+    localStorage.setItem(`id_${editObject[0][1]}`, JSON.stringify(Object.fromEntries(editObject)))
+}
+
+
+// // Cancale Editing
+function cancaleEditing(props,trAll,editIcons){
+    editIcons[1].style.display = 'inline'
+    editIcons[2].style.display = 'none'
+    editIcons[3].style.display = 'none'
+    
+    
+    Object.entries(props).map(function(elem,index) {
+        if(index > 1 && index < 7) {
+            trAll[index-2].lastElementChild.textContent = elem[1]
+            return elem
+        }
+        else return elem 
+    })
 }
 
 // Submit kochakne
@@ -175,6 +173,8 @@ form.addEventListener("submit", function(e){
     e.preventDefault()
     fo["id"] = id
     fo["img"] = form[7].files[0] ? URL.createObjectURL(form[7].files[0]) : 'img/people3.png';
+    // console.log(URL.createObjectURL(form[7].files[0]))
+    // console.log(form[7].files)
     for(let i of form.children){
         if(i.name) fo[i.name] = i.value
     }
@@ -191,26 +191,24 @@ form.addEventListener("submit", function(e){
     addTable(fo)
 })
 
+
 // Functionn e or@ kavelcne tr,td and text td-neri mej
 function addTable(fo) {
     const tr = document.createElement("tr")
     for(let i in fo){
         const td = document.createElement("td");
-        if(i == 'data' || i == 'time' || i == 'old' || i == "edit") continue
-        // console.log(i)
-        if(i != "img") td.textContent = fo[i]
+        if(i == 'data' || i == 'time' || i == 'old' || i == "edit" || i == 'company') continue
+        if(i != "img") {
+            td.textContent = fo[i]
+        }
         if(i == "edit") icons("edit",td)
         else if( i == "delet") icons("delet",td)
         else if( i == "info") icons("info",td)
         else if(i == "img") {
             const userImg = document.createElement("img");
-            // userImg.setAttribute("src", `img/${fo[i]}`)
-            // URL.revokeObjectURL(userImg)
 
             URL.revokeObjectURL(userImg.src)
             userImg.src = fo[i]
-
-
             userImg.classList.add("userImg")
             td.appendChild(userImg)
         }
@@ -246,7 +244,7 @@ document.addEventListener('DOMContentLoaded', function(){
 // Clear table and LocalStrong 
 const clear = document.querySelector(".clearTable")
 clear.addEventListener("click", function(e){
-    let id = !localStorage.getItem("idEnd") ? 1 : localStorage.getItem("idEnd")
+    id = !localStorage.getItem("idEnd") ? 1 : localStorage.getItem("idEnd")
     // id = 1
     localStorage.clear()
     table.textContent = ""
